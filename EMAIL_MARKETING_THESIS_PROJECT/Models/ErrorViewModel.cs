25028ea03d1c434bce5412abe20bd16e37a1920a
daominhhieu97 <hieudm97@gmail.com>
2019-11-04 20:12:32 +0700

    Initial Project
    Added needed file.

---
+++ EMAIL_MARKETING_THESIS_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EMAIL_MARKETING_THESIS_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EMAIL_MARKETING_THESIS_PROJECT/Models/Scheduler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMAIL_MARKETING_THESIS_PROJECT.Models
+{
+    public class Scheduler
+    {
+    }
+}
